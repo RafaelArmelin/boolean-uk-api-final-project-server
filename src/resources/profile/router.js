7f36e4f9ed5fd8fdb139466c2d6c8242ccce057c
Rafael Armelin <raffael.armelin@gmail.com>
2021-10-26 16:29:15 +0100

    update profile not working

--- src/resources/profile/router.js
+++ src/resources/profile/router.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
-const { createOne } = require("./controller");
+const { createOne, updateOneById } = require("./controller");
 
 const router = express.Router();
 
 router.post("/", createOne);
+
+router.put("/:id", updateOneById);
 
 module.exports = router;
