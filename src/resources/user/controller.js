e02186735cf1d008d4afbdf3136dc250d1a2534a
Rafael Armelin <raffael.armelin@gmail.com>
2021-10-28 12:57:55 +0100

    adding get all to users controller

--- src/resources/user/controller.js
+++ src/resources/user/controller.js
@@ -19,6 +19,21 @@ const createOne = async (req, res) => {
   }
 };
 
+const getAllUsers = async (req, res) => {
+  console.log("req.body: ", req.body);
+
+  try {
+    const result = await prisma.user.findMany();
+
+    res.json({ users: result });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({ error: error });
+  }
+};
+
 module.exports = {
-  createOne
+  createOne,
+  getAllUsers,
 };
